1581b0bd4a2662b7e455fde2356ee6b488b1a214
Neal Sanche <neal.sanche@robotsandpencils.com>
2017-02-28 15:28:22 -0700

    Initial commit

---
+++ app/src/main/java/com/robotsandpencils/databindingprimer/ViewModel.java
@@ -0,0 +1,15 @@
+package com.robotsandpencils.databindingprimer;
+
+import android.view.View;
+
+import timber.log.Timber;
+
+/**
+ * Created by nealsanche on 2017-02-28.
+ */
+
+public class ViewModel {
+    public void onClick() {
+        Timber.d("ViewModel onClick");
+    }
+}
